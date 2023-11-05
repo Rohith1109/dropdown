0d2c16f20dcd7becfaeabba3d60dabac7dcd17e9
Rohith1109 <135093499+Rohith1109@users.noreply.github.com>
2023-11-05 16:47:38 +0530

    Add files via upload

---
+++ dropdown.js
@@ -0,0 +1,16 @@
+function select_color(){
+    var select=document.getElementById
+    ("abc").value;
+
+    if(select=="Red"){
+        document.body.style.backgroundColor="Red"
+    }
+
+    if(select=="Blue"){
+        document.body.style.backgroundColor="Blue"
+    }
+
+    if(select=="Green"){
+        document.body.style.backgroundColor="Green"
+    }
+}
